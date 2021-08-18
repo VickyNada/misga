e521bb5ac49a3bd3ec12d3aad39178ec45da982e
HarshaKumara11 <madushanka.harsha69@gmail.com>
2021-08-18 23:57:04 +0530

    inventory wastage module

--- application/controllers/inventory.php
+++ application/controllers/inventory.php
@@ -8,7 +8,7 @@ class Inventory extends CI_Controller
         parent::__construct();
         $this->load->helper('form');
         $this->load->model('mcrud');
-       
+        $this->load->model('inventory_model');
     }
 
     public function index()
@@ -110,14 +110,187 @@ class Inventory extends CI_Controller
         redirect(URL_BASE . 'inventory');
 	}
     
-
-
     public function page()
 	{
 		$this->load->view('includes/page');
 	}
 
-    
+// Item Wastage Reason----------
+    // Load Wastage-Reason page
+    public function wastageReason()
+	{
+		$userid = $this->session->userdata('userid');
+		$data["userInfo"] = $this->mcrud->getDataById('users', $userid, 'id');
+		$this->load->view('includes/dash_header');
+		$this->load->view('includes/navigation', $data);
+		$this->load->view('inventory/wastageReason');
+		$this->load->view('includes/dash_footer');
+	}
+
+	// Fetch Wastage-Reason for Main Page Method
+	public function getWreasonData()
+	{
+		$allWreason = $this->mcrud->fetchAllData('wastagereasons', 'id');
+		$dataString = '';
+		foreach ($allWreason as $row) {
+			// if ($price->status == 0) {
+			// 	$status = "Active";
+			// 	$btnclass = "btn btn-danger btn-rounded";
+			// 	$faclass = "fa fa-lock";
+			// } else {
+			// 	$status = "Blocked";
+			// 	$btnclass = "btn btn-primary btn-rounded";
+			// 	$faclass = "fa fa-unlock-alt";
+			// }
+			$dataString .= '
+				<tr class="grade">
+				<td>' . $row	->id . '</td>
+				<td>' . $row	->wastageReason . '</td>
+				<td>' . $row 	->Description . '</td>
+				<td>' . $row	->createdDate . '</td>	
+				<td><a class="btn btn-warning btn-rounded" id="editWreasonBtn" onclick="editWreason(' . $row->id . ')" href="#" ><i class="fa fa-pencil-square-o"></i> Edit</a></td>
+				</tr>';
+		}
+		echo $dataString;
+	}
+
+	// Insert Wastage-Reason Method
+	public function addWreason()
+	{
+		$data = array(
+			'wastageReason'			=> strtoupper($_POST['addWcode']),
+			'Description' 		=> $_POST['addWdesc'],	 
+		);
+		$result = $this->mcrud->addDataByForm('wastagereasons', $data);
+		echo $result;
+	}
+
+	// Update Wastage-Reason Method
+	public function editWreason()
+	{
+		$id = array('id' => $_POST['editId']);
+		$data = array(
+			'Description' 	=> $_POST['editWdesc'],
+		);
+		
+		$result = $this->mcrud->updateDataByForm('wastagereasons', $data, $id);
+	}
+
+	// Fetch Wastage-Reason for editing Page Method
+	public function getWreasoneditdata()
+	{
+		$wrid = $_GET['id'];
+		$wrData = $this->mcrud->getDataById('wastagereasons', $wrid, 'id');
+		echo json_encode($wrData);
+	}
+
+// Item Wastage----------
+	//Load Item Wastage Page
+	public function wastage()
+	{
+		$userid = $this->session->userdata('userid');
+		$data["userInfo"] = $this->mcrud->getDataById('users', $userid, 'id');
+		$this->load->view('includes/dash_header');
+		$this->load->view('includes/navigation', $data);
+		$this->load->view('inventory/wastage');
+		$this->load->view('includes/dash_footer');
+	}
+
+	// Fetch Wastage-Record for Main Page Method
+	
+	public function getWastageData()
+	{
+		$allWastage = $this->mcrud->fetchAllData('itemwastage', 'id');
+		$dataString = '';
+		foreach ($allWastage as $row) {
+			// if ($price->status == 0) {
+			// 	$status = "Active";
+			// 	$btnclass = "btn btn-danger btn-rounded";
+			// 	$faclass = "fa fa-lock";
+			// } else {
+			// 	$status = "Blocked";
+			// 	$btnclass = "btn btn-primary btn-rounded";
+			// 	$faclass = "fa fa-unlock-alt";
+			// }
+			$dataString .= '
+				<tr class="grade">
+				<td>' . $row	->	id . '</td>
+				<td>' . $row	->	wastage_id . '</td>
+				<td>' . $row 	->	wastageReason . '</td>
+				<td>' . $row	->	Itemcode . '</td>
+				<td>' . $row	->	itemname . '</td>	
+				<td>' . $row	->	batchno . '</td>
+				<td style="text-align:right">' . $row	->	qty . '</td>		
+				<td style="text-align:right">' . $row	->	perItemCost . '</td>	
+				<td style="text-align:right">' . $row	->	totalAmoun . '</td>	
+				<td>' . $row	->	Description . '</td>	
+				<td>' . $row	->	createdDate . '</td>	
+				</tr>';
+		}
+		echo $dataString;
+	}
+
+	// Insert Wastage-Record Method
+	public function addWastage()
+	{
+			$data = array(
+			'wastage_id'			=> strtoupper($_POST['Worderid']),
+			'wastageReason' 		=> $_POST['Wreason'],	 
+			'Itemcode'				=> $_POST['WitemCode'],
+			'itemname'				=> $_POST['Witemdesc'],
+			'batchno'				=> $_POST['Wbatch'],
+			'qty'					=> $_POST['Wqty'],
+			'perItemCost'			=> $_POST['Wcost'],
+			'totalAmoun'			=> $_POST['Wtotal'],
+			'Description'			=> $_POST['Wremarks'],
+			);
+
+		$result = $this->mcrud->addDataByForm('itemwastage', $data);
+		echo $result;
+
+			$data2 = array(
+			'CurrentNumber'  => $_POST['woid'],
+			);
+
+		$result2 = $this->mcrud->updateDataByForm('nextnumber', $data2, array('Attribute' => 'ITEMWASTAG'));
+		// return $result2;
+
+			$batch = array('batchno' => ($_POST['Wbatch']));
+			$data3 = array(
+			'quantity'  => $_POST['Remqty'],
+			'amount'  => $_POST['Remval'],
+			);
+		
+		$result3 = $this->mcrud->updateDataByForm('onhand_stock', $data3, $batch);
+		return $result3;
+
+	}
+
+	//Fetch Wastage Record Next Number
+	public function get_next_num(){
+
+		$nn = $this->inventory_model->get_next_num();
+		echo json_encode($nn);
+	}
+
+	// Fetch Wastage Reason for dropdown Method
+	public function get_was_res_code(){
+		$allrows = $this->inventory_model->get_was_res_code();
+		echo json_encode($allrows);
+	}
+
+	//Fetch Wastage Record Item batch#
+	public function get_item_batch($id){
+		$item = $this->inventory_model->get_item_batch($id);
+		echo json_encode($item);
+	}
+	//Fetch Wastage Record Item batch# available qty
+	public function get_item_batch_qty($id){
+		$item = $this->inventory_model->get_item_batch_qty($id);
+		echo json_encode($item);
+	}
+
+   
 
 }
 
